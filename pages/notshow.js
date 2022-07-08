eaf586cea17328f09a431a9a4570f4919d6070af
reacthcmvn <react.hcmvn@gmail.com>
2022-07-08 08:57:36 +0700

    remove notshow page

--- pages/notshow.js
+++ pages/notshow.js
@@ -6,7 +6,8 @@ import getme from "../styles/src/img/tungnoise.jpg";
 const Notshow = () => {
   return (
     <div className=" overflow-hidden h-[68vh] min-w-[35vh] rounded-l-[100vh] m-2 mr-4 p-3 border-2 border-orange-300">
-      <video autoPlay controls loop muted>
+      abc
+      {/* <video autoPlay controls loop muted>
         <source src="/demo2.mp4" type="video/mp4" />
       </video>
       <Image
@@ -15,7 +16,7 @@ const Notshow = () => {
         layout="responsive"
         objectFit="cover"
       />
-      {/* <FaCommentAlt className="text-yellow-300 text-[260px] h-[220px]" /> */}
+      <FaCommentAlt className="text-yellow-300 text-[260px] h-[220px]" /> */}
     </div>
   );
 };
